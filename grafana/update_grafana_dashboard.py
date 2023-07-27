ee3cca29f42e1252864575bf9ce3e25d2333fdee
Eli Battat <ebattat@redhat.com>
2023-07-27 19:22:59 +0300

    add fetch all dashboard

--- grafana/update_grafana_dashboard.py
+++ grafana/update_grafana_dashboard.py
@@ -1,20 +1,17 @@
 import os
-
-# Access environment variables using os.environ
-grafana_url = os.environ.get("GRAFANA_URL")
-api_key = os.environ.get("API_KEY")
-json_dashboard_path = os.environ.get("JSON_DASHBOARD_PATH", 'output.json')
-
-
-
 from grafana_operations import GrafanaOperations
 
+# Access environment variables using os.environ
+grafana_url = os.environ.get("GRAFANA_URL", '')
+api_key = os.environ.get("API_KEY", '')
+json_dashboard_path = os.environ.get("JSON_DASHBOARD_PATH", 'output.json')
 
 grafana_operations = GrafanaOperations(grafana_url=grafana_url,
                                        api_key=api_key,
                                        json_dashboard_path=json_dashboard_path)
 
 # Update generate grafana dashboard
+# for debug: grafana_operations.fetch_all_dashboards()
 grafana_operations.read_dashboard_json()
 grafana_operations.increment_dashboard_version()
 grafana_operations.write_dashboard_json()
