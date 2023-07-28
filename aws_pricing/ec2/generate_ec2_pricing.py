deea154590b3f0cc265738d5c93e71475c4a82a4
Eli Battat <ebattat@redhat.com>
2023-07-28 18:01:04 +0300

    update aws ec2 pricing

--- aws_pricing/ec2/generate_ec2_pricing.py
+++ aws_pricing/ec2/generate_ec2_pricing.py
@@ -3,7 +3,7 @@ import os
 from ec2_pricing import EC2Pricing
 
 # Access environment variables using os.environ
-output_path = os.environ.get('OUTPUT_PATH', '')
+output_path = os.environ.get('GITHUB_WORKSPACE', '')
 
 ec2_pricing = EC2Pricing(output_path=output_path)
 ec2_pricing.ec2_spot_prices()
