ad0d03919d6487bec450f12e3738c0eb0e5c9ef9
Eli Battat <ebattat@redhat.com>
2023-08-08 16:20:03 +0300

    update product versions

--- grafonnet_generator/grafana/update_main_libsonnet_versions.py
+++
@@ -1,9 +0,0 @@
-
-from update_versions_in_main_libsonnet import UpdateGrafanaLastValueMappings
-
-# Update perf main.libsonnet with last versions
-update_grafana_mappings_value = UpdateGrafanaLastValueMappings(main_libsonnet_path=main_libsonnet_path)
-last_versions = update_grafana_mappings_value.get_last_elasticsearch_versions()
-update_grafana_mappings_value.update_value_mappings_last_versions(last_versions=last_versions)
-update_grafana_mappings_value.update_main_libsonnet()
-
